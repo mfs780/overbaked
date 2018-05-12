9a683eb9c0f199558ca48a3118db84a9ebc58e4e
Mohammad Fahad Sheikh <fahadsheikh780@gmail.com>
2018-05-11 23:49:57 -0700

    Fix Trash Bug

--- src/Objects/Hole.lua
+++ src/Objects/Hole.lua
@@ -12,10 +12,12 @@ function Hole:init(def, x, y)
 end
 
 function Hole:onGrab (player, foods)
-    if (player.carrying and player.carrying.ingredients) then
+    if (player.carrying and string.sub(player.carrying.state, 1, 1) == 'p') then
+        print('OTHER', player.carrying.index)
         player.carrying.ingredients = {}
         player.carrying.state = 'pen'
     elseif (player.carrying) then
+        print('DELETE', player.carrying.index)
         table.remove(foods, player.carrying.index)
         player.carrying = nil
     end
